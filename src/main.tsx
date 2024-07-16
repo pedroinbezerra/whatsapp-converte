d749e3741e3b80ab90f64fb18048b7a57b68173c
Pedro Bezerra <pedro.inbezerra@outlook.com>
2024-07-16 04:17:17 -0300

    Implementing changelog

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.tsx';
 import './index.css';
-import { Layout, Menu, Image, Typography } from 'antd';
-import { GithubFilled, HeartFilled, LinkedinFilled, WhatsAppOutlined } from '@ant-design/icons'
+import { Layout, Menu, Image, Typography, Modal, Badge } from 'antd';
+import { CodeFilled, GithubFilled, HeartFilled, LinkedinFilled, WhatsAppOutlined } from '@ant-design/icons'
 import axios from 'axios';
 
 const { Text } = Typography;
 const { Header, Content, Footer } = Layout;
 
-const url = import.meta.env.VITE_GET_CLICKS;
+const clicksUrl = import.meta.env.VITE_GET_CLICKS;
+const changelogUrl = import.meta.env.VITE_GET_CHANGELOG;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const containerStyle: React.CSSProperties = {
@@ -42,8 +43,21 @@ const menuStyle: React.CSSProperties = {
   backgroundColor: '#054640'
 };
 
+const iconSize: React.CSSProperties = {
+  fontSize: '125%'
+};
+
 const AppLayout: React.FC = () => {
-  const [totalClicks, setTotalClicks] = useState();
+  const [totalClicks, setTotalClicks] = useState(1);
+  const [changelogModalOpen, setChangelogModalOpen] = useState(false);
+  const [localChangelogVersion, setLocalChangelogVersion] = useState(sessionStorage.getItem('changelog_version') || '');
+  const [changelog, setChangelog] = useState(['Tudo certo!']);
+  const [countChanges, setCountChanges] = useState(0);
+
+  const openChangelogModal = () => {
+    setChangelogModalOpen(true);
+    setCountChanges(0);
+  }
 
   useEffect(() => {
     const config = {
@@ -52,27 +66,49 @@ const AppLayout: React.FC = () => {
       }
     };
 
-    axios.get(url, config)
+    axios.get(clicksUrl, config)
       .then((response: any) => {
         setTotalClicks(response.data);
+      })
+
+    axios.get(changelogUrl, config)
+     .then((response: any) => {
+        const {version, changes} = response.data;
+
+        if(!localChangelogVersion || localChangelogVersion !== version) {
+          setCountChanges(changes.length || 0);
+        }
+
+        setLocalChangelogVersion(version);
+        setChangelog(changes);
+
+        sessionStorage.setItem('changelog_version', version);
       })
   }, []);
 
   const contacts = [
     {
       key: '1',
-      icon: <GithubFilled />,
+      icon: <GithubFilled style={iconSize}/>,
       onClick: () => window.location.href = 'https://github.com/pedroinbezerra'
     },
     {
       key: '2',
-      icon: <LinkedinFilled />,
+      icon: <LinkedinFilled style={iconSize}/>,
       onClick: () => window.location.href = 'https://www.linkedin.com/in/pedroinbezerra/'
     },
     {
       key: '3',
-      icon: <WhatsAppOutlined />,
+      icon: <WhatsAppOutlined style={iconSize}/>,
       onClick: () => window.location.href = 'whatsapp://send?phone=5585986701595'
+    }, 
+    {
+      key: '4',
+      icon: 
+        <Badge count={countChanges} size='small'>
+          <CodeFilled style={iconSize}/>
+        </Badge>,
+      onClick: () => openChangelogModal()
     }
   ];
 
@@ -80,24 +116,37 @@ const AppLayout: React.FC = () => {
 
   return (
     <React.StrictMode>
-      <div style={containerStyle}>
-        <Header style={headerStyle}>
-          <div className="demo-logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            items={items}
-            style={menuStyle}
-          />
-          {totalClicks && <Text style={{color: '#ffffffa6'}} ><HeartFilled /> {totalClicks}</Text>}
-        </Header>
-        <Content style={contentStyle}>
-          <App />
-        </Content>
-        <Footer style={footerStyle}>
-          <Image src='/logo.png' width={'192px'} />
-        </Footer>
-      </div>
+      <>
+        <div style={containerStyle}>
+          <Header style={headerStyle}>
+            <div className="demo-logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              items={items}
+              style={menuStyle}
+            />
+            {!!totalClicks && <Text style={{color: '#ffffffa6'}} ><HeartFilled style={iconSize}/> {totalClicks}</Text>}
+          </Header>
+          <Content style={contentStyle}>
+            <App />
+          </Content>
+          <Footer style={footerStyle}>
+            <Image src='/logo.png' width={'192px'} />
+          </Footer>
+        </div>
+      </>
+      <Modal title="Novidades" open={changelogModalOpen} onCancel={() => setChangelogModalOpen(false)} centered closable footer="">
+        <ul>
+          {
+            
+            changelog.map((change, index) => (
+              <li key={index}>{change}</li>
+            ))
+            
+          }
+        </ul>
+      </Modal>
     </React.StrictMode>
   );
 };
